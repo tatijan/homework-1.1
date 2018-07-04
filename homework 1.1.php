929b29dda046021bcd8a2259be577968592b2d91
tatijan <34741441+tatijan@users.noreply.github.com>
2018-07-04 10:03:05 +0200

    Add files via upload

---
+++ homework 1.1.php
@@ -0,0 +1,15 @@
+<?php
+$Name = 'Татьяна';
+$Age = '37';
+$Email = 'kgd.andreeva@yahoo.com';
+$City = 'Калининград';
+$About = 'Airticketing';
+?>
+<div>
+    <h1>Страница пользователя Татьяна</h1>
+    <p>Имя <strong><?= $Name ?></p>
+    <p>Возраст <strong><?= $Age ?></p>
+    <p>Электронный адрес <strong><?= $Email ?></p>
+    <p>Город <strong><?= $City ?></p>
+    <p>О себе <strong><?= $About ?></p>
+</div>
